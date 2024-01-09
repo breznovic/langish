5b90a879fb1d37a675a1454e5584d02aa957ec92
breznovic <slymarkel@gmail.com>
2024-01-09 13:08:21 +0300

    Flip animation

--- src/components/Cards/Card/Card.tsx
+++ src/components/Cards/Card/Card.tsx
@@ -2,25 +2,31 @@ import { WordType } from "../../../store/englishWords";
 import Button from "../../common/Button/Button";
 import s from "./Card.module.css";
 
-interface PropsType extends WordType {}
+type PropsType = WordType & { reverseCard: () => void; isFlipped: boolean };
 
-const Card = (props: PropsType) => {
+export const Card = (props: PropsType) => {
+  const { word, definition, reverseCard, isFlipped } = props;
+
+  const handleClick = () => {
+    reverseCard();
+  };
+
+  const cardClassName = `${s.card} ${isFlipped ? s.flipped : ""}`;
+
   return (
-    <div>
-      <div className={s.card}>
-        <h2 className={s.word}>{props.word}</h2>
-        <p className={s.definition}>{props.definition}</p>
-        <div className={s.buttonGroup}>
-          <div>
-            <Button title="I know" className={s.knowButton}/>
-          </div>
-          <div>
-            <Button title="To learn" />
-          </div>
+    <div className={cardClassName}>
+      <h2 className={s.word}>{word}</h2>
+      <p className={s.definition} onClick={handleClick}>
+        {definition}
+      </p>
+      <div className={s.buttonGroup}>
+        <div>
+          <Button title="I know" className={s.knowButton} />
+        </div>
+        <div>
+          <Button title="To learn" />
         </div>
       </div>
     </div>
   );
 };
-
-export default Card;
--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,30 +1,60 @@
 import { useSelector } from "react-redux";
-import Card from "./Card/Card";
+import { Card } from "./Card/Card";
 import { RootState } from "../../store/store";
 import s from "./Cards.module.css";
 import Header from "../Header/Header";
+import { useMemo, useState } from "react";
 
 const Cards = () => {
   const wordsForLearning = useSelector(
     (state: RootState) => state.english.englishWordsC1
   );
 
+  const [cardsBackSide, setCardsBackSide] = useState(true);
+
   const shuffledWords = wordsForLearning
     .slice()
     .sort(() => Math.random() - 0.5);
 
-  const selectedWords = shuffledWords.slice(0, 8);
+  const selectedWords = useMemo(
+    () => shuffledWords.slice(0, 8),
+    [shuffledWords]
+  );
 
-  const cards = selectedWords.map((w) => (
+  const reverseCard = (id: string) => {
+    setCardsBackSide(!cardsBackSide);
+  };
+
+  let cardsWithDefinition = selectedWords.map((w) => (
     <div key={w.id} className={s.cardWrapper}>
-      <Card id={w.id} definition={w.definition} word={w.word} />
+      <Card
+        id={w.id}
+        word={w.word}
+        definition={w.definition}
+        reverseCard={() => reverseCard(w.id)}
+        isFlipped={!cardsBackSide}
+      />
+    </div>
+  ));
+
+  let learningCards = selectedWords.map((w) => (
+    <div key={w.id} className={s.cardWrapper}>
+      <Card
+        id={w.id}
+        word={w.word}
+        definition="Click to see definition"
+        reverseCard={() => reverseCard(w.id)}
+        isFlipped={!cardsBackSide}
+      />
     </div>
   ));
 
   return (
     <div>
       <Header />
-      <div className={s.container}>{cards}</div>
+      <div className={s.container}>
+        {cardsBackSide ? learningCards : cardsWithDefinition}
+      </div>
     </div>
   );
 };
